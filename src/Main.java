eb370ca445d066da52d2e3cf7b39569c95d5a558
alexandrewolny <alexandrewolny>
2021-01-18 11:19:52 +0100

    Remove personnageFactory in Main

--- src/Main.java
+++ src/Main.java
@@ -6,16 +6,6 @@ public class Main {
 
 	}
 	
-	/**
-	* Créer un personnage avec touss ses attributs. 
-	* Demande a l'utilisateur d'entrer le nom du personnage.
-	* retour: une instance de la classe Personnage correctement instancié.
-	 * @return 
-	**/
-	public static void personnageFactory(){
-	    // Demander a l'utilisateur un nom de personnage
-	    // Creer un nouveau personnage en utilisant le constructeur avec tous ses params (dont le nom qui vient d'être choisi par l'utilisateur)
-	    // Retourner l'instance du personnage
-	}
+	
 
 }
--- src/Monde.java
+++ src/Monde.java
@@ -1,10 +1,18 @@
 
 public class Monde {
 	
+	/**
+	* Créer un personnage avec touss ses attributs. 
+	* Demande a l'utilisateur d'entrer le nom du personnage.
+	* retour: une instance de la classe Personnage correctement instancié.
+	 * @return 
+	**/
 	public static Personnage personnageFactory()
 	   {
 		return null;
-	       // TODO
+		  // Demander a l'utilisateur un nom de personnage
+	    // Creer un nouveau personnage en utilisant le constructeur avec tous ses params (dont le nom qui vient d'être choisi par l'utilisateur)
+	    // Retourner l'instance du personnage
 	   }
 
 	   public static void afficherInformations()
