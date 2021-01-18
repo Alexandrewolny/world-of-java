6fcee5dbcf58c7a4af7b1fe6893a31c5379e8e84
alexandrewolny <alexandrewolny>
2021-01-18 11:16:38 +0100

    Création de la class static Monde

---
+++ src/Monde.java
@@ -0,0 +1,14 @@
+
+public class Monde {
+	
+	public static Personnage personnageFactory()
+	   {
+		return null;
+	       // TODO
+	   }
+
+	   public static void afficherInformations()
+	   {
+	       // TODO
+	   }
+}
